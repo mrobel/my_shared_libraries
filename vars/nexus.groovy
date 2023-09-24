cb2e40c16c1f49f96d9916c399c1362fd9f44fad
M Robel Miah <47643759+mrobel@users.noreply.github.com>
2023-09-25 00:41:38 +0200

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,3 +1,3 @@
 def call() {           
-    nexusArtifactUploader artifacts: [[artifactId: '01-maven-web-app', classifier: '', file: 'target/01-maven-web-app.war', type: 'war']], credentialsId: 'Nexus-credential', groupId: 'in.robelit', nexusUrl: '54.209.117.121:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'pipeline-repo', version: '1.0-SNAPSHOT'
+    nexusArtifactUploader artifacts: [[artifactId: '01-maven-web-app', classifier: '', file: 'target/01-maven-web-app.war', type: 'war']], credentialsId: 'Nexus-credential', groupId: 'in.robelit', nexusUrl: '54.237.58.37:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'pipeline-repo', version: '1.0-SNAPSHOT'
 }
